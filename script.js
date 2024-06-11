a186c87a60c41d082bc06e1e8da755361cd63876
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-11 11:04:03 +0200

    added events

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const formInput = document.querySelectorAll("#form input");
+
+formInput.forEach(input =>{
+    input.addEventListener("blur", ()=>{
+
+        console.log(formInput.length, formInput[0].value, formInput[1].value, formInput[2].value, formInput[3].value)
+    })
+})
