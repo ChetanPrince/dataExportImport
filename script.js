dff95d4b26bdb72b53dc1cf76c374ab012ed8be2
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-11 19:26:45 +0200

    trying to create tr td to save data

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const formInput = document.querySelectorAll("#form input");
+const tbody = document.getElementById("tbody");
+
 
 const saveData = () =>{formInput.forEach(input =>{
     // input.addEventListener("blur", ()=>{
         let tableContent = "";
         for(let i=0; i<formInput.length; i++){
-            tableContent+=formInput[i].value + ",";
+            // tableContent+=formInput[i].value + ",";
+     tbody.textContent += `<tr><td>${formInput[i].value}</td></tr>` + "\n";
+            console.log();
         }
-        console.log(tableContent)
-        // create tr and td here perhaps or above it and split data and then join
+        // console.log(tableContent)
+        // create tr and td here perhaps or above it and split data and then join and then reset input value to empty strings
 formInput[0].value = "";
 formInput[1].value = "";
 formInput[2].value = "";
