d08d77f3a3cb5e491a7b3586cf49f5cf37b41f92
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-11 11:13:21 +0200

    basic grabbing data inside a funciton and resetting their values

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const formInput = document.querySelectorAll("#form input");
 
-formInput.forEach(input =>{
-    input.addEventListener("blur", ()=>{
-
-        console.log(formInput.length, formInput[0].value, formInput[1].value, formInput[2].value, formInput[3].value)
-    })
-})
+const saveData = () =>{formInput.forEach(input =>{
+    // input.addEventListener("blur", ()=>{
+        let tableContent = "";
+        for(let i=0; i<formInput.length; i++){
+            tableContent+=formInput[i].value + ",";
+        }
+        console.log(tableContent)
+        // create tr and td here perhaps or above it and split data and then join
+formInput[0].value = "";
+formInput[1].value = "";
+formInput[2].value = "";
+formInput[3].value = "";
+    // })
+})}
+const saveBtn = document.getElementById("save");
+saveBtn.addEventListener("click", saveData);
