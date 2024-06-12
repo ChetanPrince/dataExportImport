12a1e1c327cfff3b72324d718b110193bbe74203
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-12 10:58:30 +0200

    problems faced and represented for futher solution

--- script.js
+++ script.js
@@ -3,21 +3,20 @@ const tbody = document.getElementById("tbody");
 
 
 const saveData = () =>{formInput.forEach(input =>{
-    // input.addEventListener("blur", ()=>{
         let tableContent = "";
         let tr = document.createElement("tr");
         tbody.appendChild(tr);
         for(let i=0; i<formInput.length; i++){
-            
-            let col = document.createElement("td");
-            col.textContent = formInput[i].value;
-            console.log(i);
-            tr.appendChild(col);
-    //  col.innerHTML += `<td>${formInput[i].value}</td>`;
-            
+if(formInput[i].value === ""){
+    return;
+}else{
+    let col = document.createElement("td");
+    col.textContent = formInput[i].value;
+    console.log(i); 
+    tr.appendChild(col);
+    }
         }
-        // console.log(tableContent)
-        // create tr and td here perhaps or above it and split data and then join and then reset input value to empty strings
+      
 formInput[0].value = "";
 formInput[1].value = "";
 formInput[2].value = "";
