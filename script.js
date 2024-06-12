834415788719789e1ef312121528995cac11dc52
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-12 18:38:36 +0200

    checking other ways to grab and hold data

--- script.js
+++ script.js
@@ -3,19 +3,35 @@ const tbody = document.getElementById("tbody");
 
 
 const saveData = () =>{formInput.forEach(input =>{
-        let tableContent = "";
+    input.addEventListener("blur", e =>{
         let tr = document.createElement("tr");
         tbody.appendChild(tr);
-        for(let i=0; i<formInput.length; i++){
-if(formInput[i].value === ""){
-    return;
-}else{
-    let col = document.createElement("td");
-    col.textContent = formInput[i].value;
-    console.log(i); 
-    tr.appendChild(col);
-    }
-        }
+        let col = document.createElement("td");
+        tr.appendChild(col);
+
+        col += input.value;
+        console.log(col);
+})
+
+            // Now we have a problem here when first input is empty then function does not operate and when another field input is not filled then only the continuous filled input is picked and loop is run for four times for that single loop
+            // 1. stop loop to iterate 4 times
+            // 2. grab input values according to their ids#
+            // 3. match input values to their corresponding input fields
+            // 4. set them in the same order in the table
+            // 5. display table content
+
+
+
+            //Observation:   content is only representing data if all input fields are filled 
+// if(formInput[i].value === ""){
+//     return;
+// }else{
+//     let col = document.createElement("td");
+//     col.textContent = formInput[i].value;
+//     console.log(i); 
+//     tr.appendChild(col);
+//     }
+        // }
       
 formInput[0].value = "";
 formInput[1].value = "";
