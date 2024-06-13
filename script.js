708129fc73f868cefd5509b18242456be2204e10
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-13 19:31:07 +0200

    trying crud operation

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const formInput = document.querySelectorAll("#form input");
 const tbody = document.getElementById("tbody");
 
+let selectedRow = null
 
-// hardCoding formInput[0] value to sequential tds
-//  instead we need the form to save data once each input field is filled correctly and save btn is pressed then dynamically the data should correspond to its required cell.
-// firstly we need tr with th that claims their corresponding cells which should be hidden at first but after getting their first values should be visible
+function onFormSubmit(e) {
+	e.preventDefault();
+        let formData = getData();
+        if (selectedRow == null){
+            saveData(formData);
+		}
+        else{
+            updateRecord(formData);
+		}
+        resetForm();    
+}
 
 // Crud operations firstly store inputs in a variable
 function getData(){
@@ -16,14 +25,14 @@ formData["contactInput"] = document.getElementById("contact-input").value;
 return formData;
 }
 
-const saveData = (data) =>{
+function saveData(data){
     let table = document.getElementById("table").getElementsByTagName('tbody')[0];
     let newRow = table.insertRow(table.length);
 
        let td1 = newRow.insertCell(0);
        td1.innerHTML = data.nameInput;
        let td2 = newRow.insertCell(1);
-       td2.innerHTML = data.surNameInput;
+       td2.innerHTML = data.surnameInput;
        let td3 = newRow.insertCell(2);
        td3.innerHTML = data.emailInput;
        let td4 = newRow.insertCell(3);
@@ -32,5 +41,5 @@ const saveData = (data) =>{
        td5.innerHTML = `<button onClick="edit(this)">Edit</button><button onClick="delete(this)">Delete</delete>`;}
 
 
-const saveBtn = document.getElementById("save");
-saveBtn.addEventListener("click", saveData);
+// const saveBtn = document.getElementById("save");
+// saveBtn.addEventListener("click", saveData());
