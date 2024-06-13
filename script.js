73c13268dec4cc8f26669fe94998461e79c92e0f
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-13 09:51:10 +0200

    grabbing form input

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
 const formInput = document.querySelectorAll("#form input");
 const tbody = document.getElementById("tbody");
 
+// hardCoding formInput[0] value to sequential tds
+//  instead we need the form to save data once each input field is filled correctly and save btn is pressed then dynamically the data should correspond to its required cell.
+// firstly we need tr with th that claims their corresponding cells which should be hidden at first but after getting their first values should be visible
 
-const saveData = () =>{formInput.forEach(input =>{
+
+const saveData = () =>{
        let td1 = formInput[0].value,
        td2 = formInput[1].value,
        td3 = formInput[2].value,
        td4 = formInput[3].value;
-        tbody.innerHTML = `<tr><td>${td1}</td><td>${td2}</td><td>${td3}</td><td>${td4}</td></tr>`;
+        tbody.innerHTML += `<tr><td>${td1}</td><td>${td2}</td><td>${td3}</td><td>${td4}</td></tr>`;
        console.log(formInput[0].value)
-})
-
-            // Now we have a problem here when first input is empty then function does not operate and when another field input is not filled then only the continuous filled input is picked and loop is run for four times for that single loop
-            // 1. stop loop to iterate 4 times
-            // 2. grab input values according to their ids#
-            // 3. match input values to their corresponding input fields
-            // 4. set them in the same order in the table
-            // 5. display table content
-
-
-
-            //Observation:   content is only representing data if all input fields are filled 
-// if(formInput[i].value === ""){
-//     return;
-// }else{
-//     let col = document.createElement("td");
-//     col.textContent = formInput[i].value;
-//     console.log(i); 
-//     tr.appendChild(col);
-//     }
-        // }
-      
-// formInput[0].value = "";
-// formInput[1].value = "";
-// formInput[2].value = "";
-// formInput[3].value = "";
-    // })
+        formInput[0].value="";
+        formInput[1].value="";
+        formInput[2].value="";
+        formInput[3].value="";
 }
+
 const saveBtn = document.getElementById("save");
 saveBtn.addEventListener("click", saveData);
