da34e23969f88543487b8a27bfa089c041ee532e
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-14 11:03:11 +0200

    rewriting what learnt

--- script.js
+++ script.js
@@ -1,55 +1,21 @@
-const formInput = document.querySelectorAll("#form input");
-const tbody = document.getElementById("tbody");
-
-let selectedRow = null;
-
-function onFormSubmit(e) {
-	e.preventDefault();
-        let formData = getData();
-        if (selectedRow == null){
-            saveData(formData);
-		}
-        else{
-            updateRecord(formData);
-		}
-        resetForm();    
-}
-
-let formData = [];
-// Crud operations firstly store inputs in a variable
-function getData(){
-let nameInput = document.getElementById("name-input").value;
-let surnameInput= document.getElementById("surname-input").value;
-let emailInput  = document.getElementById("email-input").value;
-let contactInput = document.getElementById("contact-input").value;
- // Store the values in an object
- const data = {
-    nameInput: nameInput,
-    surnameInput: surnameInput,
-    emailInput: emailInput,
-    contactInput: contactInput
-};
-
-// Push the object into the array
-formData.push(data);
-return formData;
-}
-
-function saveData(formData){
-    let table = document.getElementById("table").getElementsByTagName("tbody")[0];
-    let newRow = table.insertRow(table.length);
-
-       let td1 = newRow.insertCell(0);
-       td1.innerHTML = formData.nameInput;
-       let td2 = newRow.insertCell(1);
-       td2.innerHTML = formData.surnameInput;
-       let td3 = newRow.insertCell(2);
-       td3.innerHTML = formData.emailInput;
-       let td4 = newRow.insertCell(3);
-       td4.innerHTML = formData.contactInput;
-       let td5 = newRow.insertCell(4);
-       td5.innerHTML = `<button onClick="edit(this)">Edit</button><button onClick="delete(this)">Delete</delete>`;}
+// Initially create a null selectedRow
 
 
-const saveBtn = document.getElementById("save");
-saveBtn.addEventListener("click", saveData());
+
+// function to trigger on click where default event is prevented formData retrieves data from getData
+//  then condition to check if the row is empty to write or not if not then record is updated or when it is null then saveData with formData as parameter is used
+// if else is finished then form is reset
+
+
+            
+            
+            // function fetchdata
+
+// function saveData where data is designed in a cell
+
+
+    // use keys array to store names of the variables and then for each key with properties key and index create new cell at the index and use formdata key to write innerHTML
+       
+    //  reset Form function is written
+    
+// finally savebtn with add event listener is pushed
