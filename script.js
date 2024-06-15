755c76a8b68c76e8401dcacca46808f17ede3d4b
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-15 10:43:51 +0200

    data can be added smoothly now

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
 let selectedRow = null;
 
-const saveBtn = document.getElementById("save");
-saveBtn.addEventListener("click", onFormSubmit);
-
-
 function onFormSubmit(e){
     e.preventDefault();
+    let formData = getData();
     if(selectedRow == null){
         saveData(formData);
-
     }
     else{
         updateForm(formData);
-
     }
-resetForm();
+    // resetForm();
 }
 
 
-function formData(){
+function getData(){
     return{
         name: document.getElementById("name-input").value,
         surname: document.getElementById("surname-input").value,
         email: document.getElementById("email-input").value,
-        contact: document.getElementById("contact-input").value,
-        }
+        contact: document.getElementById("contact-input").value
+        };
         }
 
 function saveData(formData){
     let table = document.getElementById("table").getElementsByTagName("tbody")[0];
     let row = table.insertRow(table.length);
-    let Keys = ["name", "surname", "email", "contact"];
-    Keys.forEach((key, index)=>{
-       let td = row.insertCell[index];
+    let keys = ["name", "surname", "email", "contact"];
+    keys.forEach((key, index)=>{
+       let td = row.insertCell(index);
        td.innerHTML = formData[key];
-    })
-    let actionCell = row.insertCell(row.length);
+    });
+    let actionCell = row.insertCell(keys.length);
     actionCell.innerHTML = `<button id="edit" onClick="edit(this)">Edit</button><button id="delRow" onClick="deleteRow()">Delete</button>`;
 }
 
+const saveBtn = document.getElementById("save");
+saveBtn.addEventListener("click", onFormSubmit);
