3895a1f9ed0545a3530036808ea3f7e51fced9a9
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-15 11:00:21 +0200

    selectedRow had to be reset to null after update

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function saveData(formData){
        td.innerHTML = formData[key];
     });
     let actionCell = row.insertCell(keys.length);
-    actionCell.innerHTML = `<button id="edit" onClick="edit(this)">Edit</button><button id="delRow" onClick="deleteRow()">Delete</button>`;
+    actionCell.innerHTML = `<button onClick="edit(this)">Edit</button><button onClick="deleteRow()">Delete</button>`;
 }
 function resetForm(){
     document.getElementById("name-input").value = "";
@@ -48,6 +48,15 @@ function edit(td){
     document.getElementById("email-input").value = selectedRow.cells[2].innerHTML;
     document.getElementById("contact-input").value = selectedRow.cells[3].innerHTML;
 }
+
+function updateForm(formData){
+    selectedRow.cells[0].innerHTML = formData["name"];
+    selectedRow.cells[1].innerHTML = formData["surname"];
+    selectedRow.cells[2].innerHTML = formData["email"];
+    selectedRow.cells[3].innerHTML = formData["contact"];
+    selectedRow = null;
+}
+
 
 const saveBtn = document.getElementById("save");
 saveBtn.addEventListener("click", onFormSubmit);
