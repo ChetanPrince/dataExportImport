8b9f0736058a2bf489a0593b643581e0319737ef
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-15 10:50:57 +0200

    added reset and edit function

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function onFormSubmit(e){
     else{
         updateForm(formData);
     }
-    // resetForm();
+    resetForm();
 }
 
 
@@ -32,6 +32,21 @@ function saveData(formData){
     });
     let actionCell = row.insertCell(keys.length);
     actionCell.innerHTML = `<button id="edit" onClick="edit(this)">Edit</button><button id="delRow" onClick="deleteRow()">Delete</button>`;
+}
+function resetForm(){
+    document.getElementById("name-input").value = "";
+    document.getElementById("surname-input").value = "";
+    document.getElementById("email-input").value = "";
+    document.getElementById("contact-input").value = "";
+
+}
+
+function edit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("name-input").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("surname-input").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("email-input").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("contact-input").value = selectedRow.cells[3].innerHTML;
 }
 
 const saveBtn = document.getElementById("save");
