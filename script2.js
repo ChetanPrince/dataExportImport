9c10710fd007030ab3774963e2adfd667f617961
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-20 10:44:39 +0200

    started grabbing data and passing it to display result

--- script2.js
+++ script2.js
@@ -1,1 +1,8 @@
-const firstName = document.getElementById("name-input");
+let selectedValue = null;
+
+function getData(){
+    return{firstName: document.getElementById("name-input"),
+    surname: document.getElementById("surname-input"),
+    email: document.getElementById("email-input"),
+    contact: document.getElementById("contact-input")}
+}
