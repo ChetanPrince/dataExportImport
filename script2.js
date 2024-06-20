9f161f176e7acd6146f074e85e79e4e94e3cc99d
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-20 11:09:13 +0200

    added basic submit form function

--- script2.js
+++ script2.js
@@ -2,21 +2,34 @@ let selectedValue = null;
 
 function submitForm(e){
     e.preventDefault();
+    let formData = getData();
     if(selectedValue == null){
-        saveData(getData);
+        saveData(formData);
     }
     else{
-        updateForm(getData);
+        updateForm(formData);
     }
+    resetForm();
 }
 
 function getData(){
-    return{firstName: document.getElementById("name-input"),
-    surname: document.getElementById("surname-input"),
-    email: document.getElementById("email-input"),
-    contact: document.getElementById("contact-input")}
+    return{firstName: document.getElementById("name-input").value,
+    surname: document.getElementById("surname-input").value,
+    email: document.getElementById("email-input").value,
+    contact: document.getElementById("contact-input").value}
 }
 
-function saveData(getData){
-    const table = document.querySelector("table2", tbody[0] )
+function saveData(formData){
+    const table = document.getElementById("table2").getElementsByTagName("tbody")[0];
+    const row = table.insertRow(table.length);
+    let keys = ["firstName", "surname", "email", "contact"];
+    keys.forEach((key, index)=>{
+        let td = row.insertCell(index);
+        td.innerHTML = formData[key];
+    });
+    let actionCell = row.insertCell(keys.length);
+    actionCell.innerHTML = `<button id="edit" onClick="edit()">Edit</button><button id="delete" onClick="deleteRow()">Delete</button>`;
+    
 }
+const btn = document.getElementById("save");
+btn.addEventListener("click", submitForm);
