03ad714d91692af592f19c63ba62a2f90aa36fb7
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-20 10:54:54 +0200

    added basic conce[pt of grabbing data

--- script2.js
+++ script2.js
@@ -1,8 +1,22 @@
 let selectedValue = null;
 
+function submitForm(e){
+    e.preventDefault();
+    if(selectedValue == null){
+        saveData(getData);
+    }
+    else{
+        updateForm(getData);
+    }
+}
+
 function getData(){
     return{firstName: document.getElementById("name-input"),
     surname: document.getElementById("surname-input"),
     email: document.getElementById("email-input"),
     contact: document.getElementById("contact-input")}
+}
+
+function saveData(getData){
+    const table = document.querySelector("table2", tbody[0] )
 }
