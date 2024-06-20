e6a872a5e584240734d189f2e73ed8ed8768e72f
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-20 11:41:59 +0200

    updated required fields and edit delete functions

--- script2.js
+++ script2.js
@@ -1,10 +1,15 @@
-let selectedValue = null;
+let selectedRow = null;
 
 function submitForm(e){
     e.preventDefault();
     let formData = getData();
-    if(selectedValue == null){
-        saveData(formData);
+    if(selectedRow == null){
+        if(Object.values(formData).some(value => value === "")){
+            alert("please fill required fields")
+        }
+        else{
+            saveData(formData);
+        }
     }
     else{
         updateForm(formData);
@@ -28,8 +33,37 @@ function saveData(formData){
         td.innerHTML = formData[key];
     });
     let actionCell = row.insertCell(keys.length);
-    actionCell.innerHTML = `<button id="edit" onClick="edit()">Edit</button><button id="delete" onClick="deleteRow()">Delete</button>`;
+    actionCell.innerHTML = `<button id="edit" onClick="edit(this)">Edit</button><button id="delete" onClick="deleteRow(this)">Delete</button>`;
     
+}
+
+function resetForm(){
+    document.getElementById("name-input").value = "";
+    document.getElementById("surname-input").value = "";
+    document.getElementById("email-input").value = "";
+    document.getElementById("contact-input").value = "";
+}
+
+function edit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("name-input").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("surname-input").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("email-input").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("contact-input").value = selectedRow.cells[3].innerHTML;
+}
+function updateForm(formData){
+    selectedRow.cells[0].innerHTML = formData["firstName"];
+    selectedRow.cells[1].innerHTML = formData["surname"];
+    selectedRow.cells[2].innerHTML = formData["email"];
+    selectedRow.cells[3].innerHTML = formData["contact"];
+    selectedRow = null;
+}
+function deleteRow(td){
+    if(confirm("Are you sure you want to delete entire record?")){
+        row = td.parentElement.parentElement;
+        document.getElementById("table2").deleteRow(row.rowIndex);
+        resetForm();
+    }
 }
 const btn = document.getElementById("save");
 btn.addEventListener("click", submitForm);
