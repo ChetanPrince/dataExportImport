6d0d388c7a4729731565df2545c383160acf2791
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-21 11:39:00 +0200

    rewritten exportDAta funciton

--- script2.js
+++ script2.js
@@ -68,22 +68,22 @@ function deleteRow(td){
 function exportData(){
     let rows = document.querySelectorAll("#table2 tr");
     let csvContent = "";
-    rows.forEach(row => {
+    rows.forEach(row =>{
         const cols = Array.from(row.querySelectorAll("td, th"));
         cols.pop();
         let rowsContent = Array.from(cols).map(col=> col.textContent).join(",");
         csvContent += rowsContent + "\n";
     });
-    let blob = new Blob([csvContent], {type:"text/csv"});
-    let url = window.URL.createObjectURL(blob);
+    const blob = new Blob([csvContent], {type:"text/csv"});
+    const url = window.URL.createObjectURL(blob);
     const a = document.createElement("a");
-        a.href = url;
-        a.download ="download.csv";
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        window.URL.revokeObjectURL(url);
-    }
+    a.href = url;
+    a.download = "download.csv";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+}
 
 const exportBtn = document.getElementById("export");
 exportBtn.addEventListener("click", exportData);
