edbd5fd05db3e6bba5837e2ab1a5b57a6f4f95e4
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-25 10:23:24 +0200

    need a bit more grabbing on how to identify which object element to select and how to manipulate with corresponding data

--- script2.js
+++ script2.js
@@ -20,24 +20,47 @@ function getData(){
     };
 }
 function saveData(formData){
-let table =  document.getElementById("table2").getElementsByTagName("tbody")[0];
-let rows = table.insertRow(table.length);
+let table = document.getElementById("table2").getElementsByTagName("tbody")[0];
+let newRow = table.insertRow(table.length);
 let keys = ["name", "surname", "email", "contact"];
 keys.forEach((key, index) => {
-    let td = rows.insertCell(index);
-    td.innerHTML = formData[key];
+    let cell = newRow.insertCell(newRow.length);
+    cell.innerHTML = formData[key];
 });
-let actionCell = newRow.insertCell(keys.length);
-    actionCell.innerHTML = `<button id="edit" onClick="edit(this)">Edit</button><button id="delete" onClick="deleteRow(this)">Delete</button>`;
+let actionCell = newRow.insertCell(newRow.length);
+actionCell.innerHTML = `<button id="edit" onClick="edit(this)">Edit</button><button id ="delete" onClick="deleteRow(this)">Delete</button>`;
 }
 
-function resetForm(){
-    document.getElementById("name-input").value = "";
-    document.getElementById("surname-input").value = "";
-    document.getElementById("email-input").value = "";
-    document.getElementById("contact-input").value = "";
-    selectedRow = null;
+function resetData(formData){
+        document.getElementById("name-input").value ="";
+        document.getElementById("surname-input").value = "";
+        document.getElementById("email-input").value ="";
+        document.getElementById("contact-input").value = "";
 }
+
+function edit(td){
+selectedRow = td.parentElement.parentElement;
+        document.getElementById("name-input").value = selectedRow.cells[0].innerHTML;
+        document.getElementById("surname-input").value = selectedRow.cells[1].innerHTML;
+        document.getElementById("email-input").value =selectedRow.cells[2].innerHTML;
+        document.getElementById("contact-input").value = selectedRow.cells[3].innerHTML;
+}
+
+function deleteRow(td){
+    if(confirm("Are you sure you want to delete this record")){
+        row = td.parentElement.parentElement;
+        document.getElementById("table2").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function updateData(formData){
+       selectedRow.cells[0].innerHTML = formData["name"];
+       selectedRow.cells[1].innerHTML = formData["surname"];
+       selectedRow.cells[2].innerHTML = formData["email"];
+       selectedRow.cells[3].innerHTML = formData["contact"];
+       selectedRow = null;
+}
+
 
 
 
