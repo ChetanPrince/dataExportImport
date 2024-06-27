d3322e3533431a0b744aabc0cec632d589a14864
ChetanPrince <saini.rajvir2@gmail.com>
2024-06-27 19:13:32 +0200

    done importin sections

--- script2.js
+++ script2.js
@@ -28,7 +28,7 @@ function getData(){
             td.innerHTML = formData[key];
         });
         let actionCell = newRow.insertCell(newRow.length);
-        actionCell.innerHTML = `<button id="edit" onClick="edit(this)">Edit</button><button id="delete" onClick="delete(this)">Delete</button>`;
+        actionCell.innerHTML = `<button id="edit" onClick="edit(this)">Edit</button><button id="delete" onClick="deleteRow(this)">Delete</button>`;
     }
     function resetForm(formData){
         document.getElementById("name-input").value="";
@@ -44,13 +44,66 @@ function getData(){
         document.getElementById("email-input").value=selectedRow.cells[2].innerHTML;
         document.getElementById("contact-input").value=selectedRow.cells[3].innerHTML;
     }
-    function updateForm(){
-        selectedRow.cells[0].innerHTML = document.getElementById("name-input").value;
-        selectedRow.cells[1].innerHTML = document.getElementById("surname-input").value;
-        selectedRow.cells[2].innerHTML = document.getElementById("email-input").value;
-        selectedRow.cells[3].innerHTML = document.getElementById("contact-input").value;
+    function updateForm(formData){
+        selectedRow.cells[0].innerHTML = formData["name"];
+        selectedRow.cells[1].innerHTML = formData["surname"];
+        selectedRow.cells[2].innerHTML = formData["email"];
+        selectedRow.cells[3].innerHTML = formData["contact"];
         selectedRow = null;
     }
+    function deleteRow(td){
+        row = td.parentElement.parentElement;
+        if(confirm("Are you sure you want to delete this record")){
+            document.getElementById("table2").deleteRow(row.rowIndex);
+            resetForm();
+        };
+    }
+
+
+    function exportData(){
+
+    }
+    function importData(file){
+        let reader = new FileReader();
+        reader.onload = function (event){
+            let text = event.target.result;
+            let rows = text.split("\n").map(row => row.split(","));
+            let table = document.getElementById("table2").getElementsByTagName("tbody")[0];
+            table.innerHTML = "";
+            rows.forEach(row => {
+                if(row.length >=4){
+                    let newRow = table.insertRow();
+                    row.forEach((cell, index) =>{
+                        let td = newRow.insertCell(index);
+                        td.innerHTML = cell;
+                    });
+                    if(row.length === 5){
+                        let actionCell = newRow.cells(newRow.cells.length-1);
+                        let actions = actionCell.innerHTML.split(" ");
+                        if(actions >= 2){
+                            
+                        }
+                    }
+                }
+            })
+            }
+
+    }
+
+
+    const exportBtn = document.getElementById("export");
+    exportBtn.addEventListener("click", exportData);
+    
+    const importBtn = document.getElementById("import");
+    exportBtn.addEventListener("click", ()=>{
+        const csvFileInput = document.getElementById("csvFileInput");
+        let file = csvFileInput.files[0];
+        if(file){
+            importData(file);
+        };
+
+    });
+
 
     const saveBtn = document.getElementById("save");
     saveBtn.addEventListener("click", onSubmitForm);
