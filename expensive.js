4459821cec383147d5d637ce7a49c9a505104809
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-23 18:37:58 +0200

    2

--- expensive.js
+++ expensive.js
@@ -1,18 +1,29 @@
 const button = document.getElementById("addExpense");
-button.addEventListener("click", ()=>{
-   console.log(getValues());
+button.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let formData = getValues();
+  saveData(formData);
 })
 
-
+function saveData(formData){
+    let table = document.getElementById("table").getElementsByTagName("tbody")[0];
+    let row = table.insertRow(table.length);
+    let keys = ["name", "value", "type", "time"];
+    keys.forEach((key, index)=>{
+       let td = row.insertCell(index);
+       td.innerHTML = formData[key];
+    });
+    let actionCell = row.insertCell(keys.length);
+    actionCell.innerHTML = `<button onClick="edit(this)">Edit</button><button onClick="deleteRow()">Delete</button>`;
+}
 
 function getValues(){
     return {
-
          name : document.getElementById("name").value,
          value : document.getElementById("value").value,
          type : document.getElementById("type").value,
         time : document.getElementById("time").value
-    }
+    };
 }
 
 
